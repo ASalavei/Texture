a07b8dcad34fb63c176304d8d37678845a596172
Dima Loktev <muakacho+github@gmail.com>
2019-04-18 12:10:00 +0300

    Enable AS_IG_LIST_KIT to make it available via Carthage

--- Source/Base/ASAvailability.h
+++ Source/Base/ASAvailability.h
@@ -80,7 +80,7 @@
 #endif
 
 #define AS_PIN_REMOTE_IMAGE __has_include(<PINRemoteImage/PINRemoteImage.h>)
-#define AS_IG_LIST_KIT __has_include(<IGListKit/IGListKit.h>)
+#define AS_IG_LIST_KIT 1
 
 /**
  * For IGListKit versions < 3.0, you have to use IGListCollectionView.
